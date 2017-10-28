6a5bf78b4f4ceb889103f3cd81f17add53444c9f
Daniel Duckworth <duckworthd@gmail.com>
2017-10-29 01:57:29 +0900

    storage_api: initial commit

---
+++ storage_api/storage/__init__.py
@@ -0,0 +1,8 @@
+"""Storage backends for image datasets."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from storage.disk import DiskDataset
+from storage.disk import LockTimeoutException
