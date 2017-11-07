3649b88769c6bf2e667b154a7bd165f7f689ab56
Daniel Duckworth <duckworthd@gmail.com>
2017-11-06 23:05:42 -0800

    features_api: remove storage

--- features_api/storage/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Storage backends for image datasets."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from storage.disk import DiskDataset
